23f0d3603f3811f35417cd9a5662f3e1f39604b1
sophiegr02 <sophie@broadinstitute.org>
2021-04-22 17:14:17 -0400

    folders removed and final version of stats

--- scripts/stats_std.py
+++ scripts/stats_std.py
@@ -17,22 +17,21 @@ def mean_of_array(reads,start, pos):
 	return mean
 
 def main():
-	mybam = pysam.AlignmentFile('MGH283_align_sort.bam')
+	mybam = pysam.AlignmentFile('UMB.aligned.sorted.bam')
 
-	file = 'output_std_mgh_contig.txt'
+	file = 'output_std_umb_contig_final.txt'
 
 	with open(file, "w") as f:
 		print("Index Chrom Pos Thresh Diff", file=f)
 		for contig in mybam.header.references:  # Do the analysis per contig
 			stats = pysamstats.load_coverage(mybam, chrom=contig)
-
 			reads = stats.reads_pp
 			position = stats.pos
 			chrom = stats.chrom
 
-			print(len(reads))
-			print(len(position))
-			print(len(chrom))
+			#print(len(reads))
+			#print(len(position))
+			#print(len(chrom))
 
 			thresh_up, thresh_down = threshold(reads)
 			#print(thresh_up)
@@ -41,34 +40,24 @@ def main():
 			start = 0
 			big_start = 0
 			last_end = 0
+			state = "base"
 
 			for i in range(1000, len(reads), 1000):
-				if i == ((len(reads)) - 1):
-					current = reads[i]
-					next_one = reads[0]
-					next_pos = position[0]
-				else:
-					current = reads[i]
-					next_one = reads[i+1]
-					next_pos = position[i+1]
-
 				mean = mean_of_array(reads, start, i)
 				if mean >= thresh_up:
-					if last_end == start:
-						big_start_chrom = str(chrom[big_start]).lstrip('b\'').rstrip('\'')
-						i_chrom = str(chrom[i]).lstrip('b\'').rstrip('\'')
-						print(f'{big_start:<15} {i:<15} {mean:<10} +3sigma {big_start_chrom:>20}:{position[big_start]} {i_chrom:>20}:{position[i]}')
-						print(f'{big_start:<15} {i:<15} {mean:<10} +3sigma {big_start_chrom:>20}:{position[big_start]} {i_chrom:>20}:{position[i]}', file=f)
-					else:
-						start_chrom = str(chrom[start]).lstrip('b\'').rstrip('\'')
-						i_chrom = str(chrom[i]).lstrip('b\'').rstrip('\'')
-						print(f'{start:<15} {i:<15} {mean:<10} +3sigma {start_chrom:>20}:{position[start]} {i_chrom:>20}:{position[i]}')
-						print(f'{start:<15} {i:<15} {mean:<10} +3sigma {start_chrom:>20}:{position[start]} {i_chrom:>20}:{position[i]}', file=f)
+					if last_end != start:
 						big_start = start
 					last_end = i
+					state = "on"
+					prev_i = i
+				else: 
+					if state == "on":
+						print(f'{chrom[big_start].decode("utf-8")}:{position[big_start]} {chrom[prev_i].decode("utf-8"):>20}:{position[prev_i]}')
+						print(f'{chrom[big_start].decode("utf-8")}:{position[big_start]} {chrom[prev_i].decode("utf-8"):>20}:{position[prev_i]}', file=f)
+						state = "off"
+					else:
+						state = "off"
 				start = i
-
-	c1 = subprocess.Popen(["tac {} | sort -u -k 1,1 | tac".format(file)], shell=True, stdout=open("output_std_mgh_contig_final.txt", "w"))
 
 if __name__ == '__main__':
     main()
